241474ba1e6056e4df4a4a48a3477f29fadfe02e
ParnikaSarbahi <144117040+ParnikaSarbahi@users.noreply.github.com>
2025-02-23 14:53:23 +0530

    Update EncapsulationClass.java

--- EncapsulationClass.java
+++ EncapsulationClass.java
@@ -1,5 +1,5 @@
 package pack;
-public class Student{
+class Student{
   private String name;//private data member
   public String getName(){//getter method for name
     return name;
@@ -8,4 +8,14 @@ public class Student{
   public void setName(String name){
     this.name=name;
   }
+}
+public class Test{
+public static void main(String[] args){
+//creating instance of the encapsulated class
+Student s=new Student();
+//setting value in the name member
+s.setName("vijay");
+//getting value of the name member
+System.out.println(s.getName());
+}
 }
