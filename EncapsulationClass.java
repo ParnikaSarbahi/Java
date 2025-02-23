dab636249ee278f4f58c37c3bb3ee81fb20730cd
ParnikaSarbahi <144117040+ParnikaSarbahi@users.noreply.github.com>
2025-02-23 14:51:29 +0530

    Create EncapsulationClass.java

---
+++ EncapsulationClass.java
@@ -0,0 +1,11 @@
+package pack;
+public class Student{
+  private String name;//private data member
+  public String getName(){//getter method for name
+    return name;
+  }
+  //setter method for name
+  public void setName(String name){
+    this.name=name;
+  }
+}
